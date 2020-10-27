6dc74814fe0a5ce0492a491a656e87d754616925
Milen Iliev <MIliev@verisk3e.com>
2020-10-27 15:42:34 +0200

    Initial commit

---
+++ spec/language-expert-rules-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageExpertRulesView from '../lib/language-expert-rules-view';
+
+describe('LanguageExpertRulesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
